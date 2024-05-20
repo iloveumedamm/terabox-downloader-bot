b261d9d9949aae7b25fda817883a12c9ce40bf20
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-05-20 23:21:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
 Join @RoldexVerse For Updates
 [Source Code](https://github.com/r0ld3x/terabox-downloader-bot) """
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb1", m.peer_id)
     if not check_if:
         return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb4", m.peer_id)
     if not check_if:
         return await m.reply(
             "Please join @RoldexVerseChats then send me the link again."
@@ -69,10 +69,10 @@ Join @RoldexVerse For Updates
 async def start(m: UpdateNewMessage):
     text = m.pattern_match.group(1)
     fileid = db.get(str(text))
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb1", m.peer_id)
     if not check_if:
         return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb4", m.peer_id)
     if not check_if:
         return await m.reply(
             "Please join @RoldexVerseChats then send me the link again."
@@ -126,10 +126,10 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb1", m.peer_id)
     if not check_if:
         return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@streaamdb4", m.peer_id)
     if not check_if:
         return await m.reply(
             "Please join @RoldexVerseChats then send me the link again."
